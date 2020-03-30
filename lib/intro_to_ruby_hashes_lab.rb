f15531e55a36f715c747926885d955f4fc0a07e1
Zakari Badiru <zbadiru@live.co.uk>
2020-03-30 16:54:27 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,28 @@
 def new_hash
+  { }
   # return an empty hash
 end
 
 def my_hash
+  hash1 = {
+  :favorite_color => "blue"
+  }
+  hash1
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  hash = {
+    :name => "Grace Hopper"
+  }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  hash = {
+    :id => number
+  }
+  hash
   # return a hash with a key :id assigned to the provided number
 end
